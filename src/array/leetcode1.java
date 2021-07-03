3d56bc5e24884736ad44fe2f5449117d96b2ecd7
Eric Yin <ericyin2016@gmail.com>
2021-07-03 20:49:35 +0800

    leetcode1

--- src/array/leetcode1.java
+++ src/array/leetcode1.java
@@ -1,8 +1,21 @@
 package array;
 
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
+
 // TODO 两数之和 https://leetcode-cn.com/problems/two-sum/
 public class leetcode1 {
+    // TODO 暴力 O（n^2）
+    // TODO hashtable O（n）
     public int[] twoSum(int[] nums, int target) {
-        
+        Map<Integer, Integer> hashtable = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (hashtable.containsKey(target - nums[i])) {
+                return new int[]{hashtable.get(target - nums[i]), i};
+            }
+            hashtable.put(nums[i], i);
+        }
+        return new int[0];
     }
 }
