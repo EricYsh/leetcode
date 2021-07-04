2eb85172031c11d616e65e2e26098c1772a7d7f5
Eric Yin <ericyin2016@gmail.com>
2021-07-04 19:58:38 +0800

    leetcode66

---
+++ src/array/leetcode66.java
@@ -0,0 +1,16 @@
+package array;
+
+// TODO 加一 https://leetcode-cn.com/problems/plus-one/
+public class leetcode66 {
+    public int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            digits[i]++;
+            digits[i] %= 10;
+            if (digits[i] != 0)
+                return digits;
+        }
+        digits = new int[digits.length + 1];
+        digits[0] = 1;
+        return digits;
+    }
+}
