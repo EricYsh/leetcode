7cea311ff3f735c879e867634ce583e3a3d4631d
Eric Yin <ericyin2016@gmail.com>
2021-07-14 21:09:35 +0800

    leetcode242

---
+++ src/hashAndMapAndSet/leetcode242.java
@@ -0,0 +1,20 @@
+package hashAndMapAndSet;
+
+//TODO 242. 有效的字母异位词 https://leetcode-cn.com/problems/valid-anagram/description/
+public class leetcode242 {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        int[] alpha = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            alpha[s.charAt(i) - 'a']++;
+            alpha[t.charAt(i) - 'a']--;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (alpha[i] != 0) return false;
+        }
+
+        return true;
+    }
+}
