6fd1e3710788371a308e9c36f9cb1a1c32363dd9
Eric Yin <ericyin2016@gmail.com>
2021-07-17 16:49:03 +0800

    leetcode49

--- src/array/leetcode1.java
+++ src/array/leetcode1.java
@@ -7,7 +7,7 @@ import java.util.Map;
 // TODO 两数之和 https://leetcode-cn.com/problems/two-sum/
 public class leetcode1 {
     // TODO 暴力 O（n^2）
-    // TODO hashtable O（n）
+    // TODO hashtable O（n）target = a + b, for each a, if (target - a) exists in array
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> hashtable = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
---
+++ src/hashAndMapAndSet/leetcode49.java
@@ -0,0 +1,19 @@
+package hashAndMapAndSet;
+
+import java.util.*;
+
+// TODO 49. 字母异位词分组 https://leetcode-cn.com/problems/group-anagrams/
+public class leetcode49 {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] strArray = str.toCharArray();
+            Arrays.sort(strArray);
+            String key = new String(strArray);
+            List<String> list = map.getOrDefault(key, new ArrayList<String>());
+            list.add(str);
+            map.put(key, list);
+        }
+        return new ArrayList<List<String>>(map.values());
+    }
+}
