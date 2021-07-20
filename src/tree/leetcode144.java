a0fc35c3aa991284a2efa0b6c83f22ef5410827d
Eric Yin <ericyin2016@gmail.com>
2021-07-20 21:25:05 +0800

    leetcode144

---
+++ src/tree/leetcode144.java
@@ -0,0 +1,42 @@
+package tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//TODO 144. 二叉树的前序遍历 https://leetcode-cn.com/problems/binary-tree-preorder-traversal/
+public class leetcode144 {
+    // TODO 为什么树的面试题大多都是递归？取决于树的结构。
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        preOrder(root, res);
+        return res;
+    }
+
+    private void preOrder(TreeNode root, List<Integer> res) {
+        if (root == null) return;
+        res.add(root.val);
+        preOrder(root.left, res);
+        preOrder(root.right, res);
+    }
+
+
+    // Definition for a binary tree node.
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
