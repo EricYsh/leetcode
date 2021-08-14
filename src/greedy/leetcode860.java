cebe1ba040b1f5705cc41ba226f8f899704a2d3d
Eric Yin <ericyin2016@gmail.com>
2021-08-14 16:02:09 +0800

    leetcode860

---
+++ src/greedy/leetcode860.java
@@ -0,0 +1,31 @@
+package greedy;
+
+//TODO 860. 柠檬水找零 https://leetcode-cn.com/problems/lemonade-change/description/
+public class leetcode860 {
+    // greedy
+    public boolean lemonadeChange(int[] bills) {
+
+        int five = 0, ten = 0;
+        for (int bill : bills) {
+            if (bill == 5) {
+                five++;
+            } else if (bill == 10) {
+                if (five == 0) {
+                    return false;
+                }
+                five--;
+                ten++;
+            } else {
+                if (five > 0 && ten > 0) {
+                    five--;
+                    ten--;
+                } else if (five >= 3) {
+                    five -= 3;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
