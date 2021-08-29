d640a54a592c56718dcbbbb5ccde4654e7da6d62
Eric Yin <ericyin2016@gmail.com>
2021-08-29 16:15:27 +0800

    leetcode208

---
+++ src/Trie/Trie.java
@@ -0,0 +1,62 @@
+package Trie;
+
+// 208. 实现 Trie (前缀树) https://leetcode-cn.com/problems/implement-trie-prefix-tree/#/description
+public class Trie {
+    //字典树，即 Trie 树，又称单词查找树或键树，是一种树形结构。
+    // 典型应用是用于统计和排序大量的字符串（但不仅限于字符串），所以经常被搜索引擎系统用于文本词频统计。
+    //Trie 树的核心思想是空间换时间
+    /**
+     * Your Trie object will be instantiated and called as such:
+     * Trie obj = new Trie();
+     * obj.insert(word);
+     * boolean param_2 = obj.search(word);
+     * boolean param_3 = obj.startsWith(prefix);
+     */
+    private Trie[] children;
+    private boolean isEnd;
+
+    /** Initialize your data structure here. */
+    public Trie() {
+        children = new Trie[26];
+        isEnd = false;
+    }
+
+    /** Inserts a word into the trie. */
+    public void insert(String word) {
+        Trie node = this;
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+            int index = ch - 'a';
+            if (node.children[index] == null) {
+                node.children[index] = new Trie();
+            }
+            node = node.children[index];
+        }
+        node.isEnd = true;
+    }
+
+    /** Returns if the word is in the trie. */
+    public boolean search(String word) {
+        Trie node = searchPrefix(word);
+        return node != null && node.isEnd;
+    }
+
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    public boolean startsWith(String prefix) {
+        return searchPrefix(prefix) != null;
+    }
+
+    // helper method
+    private Trie searchPrefix(String prefix) {
+        Trie node = this;
+        for (int i = 0; i < prefix.length(); i++) {
+            char ch = prefix.charAt(i);
+            int index = ch - 'a';
+            if (node.children[index] == null) {
+                return null;
+            }
+            node = node.children[index];
+        }
+        return node;
+    }
+}
