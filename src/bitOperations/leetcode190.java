bfdd04f3c712c18250b3fee1ec4a93ae59e7d304
Eric Yin <ericyin2016@gmail.com>
2021-09-11 21:45:27 +0800

    leetcode190

---
+++ src/bitOperations/leetcode190.java
@@ -0,0 +1,22 @@
+package bitOperations;
+
+// 190. 颠倒二进制位 https://leetcode-cn.com/problems/reverse-bits/
+public class leetcode190 {
+    //若要翻转一个二进制串，可以将其均分成左右两部分，对每部分递归执行翻转操作，然后将左半部分拼在右半部分的后面，即完成了翻转。
+    //
+    //由于左右两部分的计算方式是相似的，利用位掩码和位移运算，我们可以自底向上地完成这一分治流程。
+    private static final int M1 = 0x55555555; // 01010101010101010101010101010101
+    private static final int M2 = 0x33333333; // 00110011001100110011001100110011
+    private static final int M4 = 0x0f0f0f0f; // 00001111000011110000111100001111
+    private static final int M8 = 0x00ff00ff; // 00000000111111110000000011111111
+
+    // wow!?
+    public int reverseBits(int n) {
+        n = n >>> 1 & M1 | (n & M1) << 1;
+        n = n >>> 2 & M2 | (n & M2) << 2;
+        n = n >>> 4 & M4 | (n & M4) << 4;
+        n = n >>> 8 & M8 | (n & M8) << 8;
+        return n >>> 16 | n << 16;
+    }
+}
+
