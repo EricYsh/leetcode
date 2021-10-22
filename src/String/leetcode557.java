2642ed1406a7023a19416a595fdddaa319dadfa8
Eric Yin <ericyin2016@gmail.com>
2021-10-22 15:36:50 +0800

    leetcode151

---
+++ src/String/leetcode557.java
@@ -0,0 +1,24 @@
+package String;
+
+// 557. 反转字符串中的单词 III https://leetcode-cn.com/problems/reverse-words-in-a-string-iii/
+public class leetcode557 {
+    public String reverseWords(String s) {
+        StringBuffer ret = new StringBuffer();
+        int length = s.length();
+        int i = 0;
+        while (i < length) {
+            int start = i;
+            while (i < length && s.charAt(i) != ' ') {
+                i++;
+            }
+            for (int p = start; p < i; p++) {
+                ret.append(s.charAt(start + i - 1 - p));
+            }
+            while (i < length && s.charAt(i) == ' ') {
+                i++;
+                ret.append(' ');
+            }
+        }
+        return ret.toString();
+    }
+}
