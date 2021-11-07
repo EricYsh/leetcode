4093959602f568e105fc98de1fd12c58c759ac7b
Eric Yin <ericyin2016@gmail.com>
2021-11-07 19:08:19 +0800

    leetcode32

---
+++ src/String/leetcode32.java
@@ -0,0 +1,27 @@
+package String;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+// 32. 最长有效括号 https://leetcode-cn.com/problems/longest-valid-parentheses/
+public class leetcode32 {
+    // solved by stack
+    public int longestValidParentheses(String s) {
+        int maxans = 0;
+        Deque<Integer> stack = new LinkedList<Integer>();
+        stack.push(-1);
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else {
+                stack.pop();
+                if (stack.isEmpty()) {
+                    stack.push(i);
+                } else {
+                    maxans = Math.max(maxans, i - stack.peek());
+                }
+            }
+        }
+        return maxans;
+    }
+}
